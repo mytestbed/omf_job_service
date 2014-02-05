29c8110e80e8d91edcbe08deadaa91d99d08b623
Max Ott <max.ott@nicta.com.au>
2014-02-06 09:44:08 +1100

    Commented out some code which isn't used anymore but may give some insight for those developing hte scheduler further

--- lib/omf/job_service/scheduler/simple_scheduler.rb
+++ lib/omf/job_service/scheduler/simple_scheduler.rb
@@ -10,34 +10,34 @@ module OMF::JobService
         warn "Received new job '#{job}', but don't know yet what to do"
       end
 
-      def schedule(res_properties, job)
-        res_properties.each {|r| schedule_single(r, job)}
-      end
-
-      def schedule_single(res_property, job)
-        rd = res_property.resource_description
-        unless type = rd[:type] || rd['type']
-          raise "Missing resource type in '#{rd}'"
-        end
-        res = nil
-        case type.to_sym
-        when :node
-          require 'omf-sfa/resource/node'
-          res = OMF::SFA::Resource::Node.create(name: res_property.name)
-        else
-          raise "Unsupported resource type '#{type}'"
-        end
-
-        unless res
-          raise "Couldn't create resource '#{rd}'"
-        end
-        puts "RES>>> #{res}"
-        res.job = job
-        res.save
-        res_property.resource = res
-        res_property.save
-
-      end
+      # def schedule(res_properties, job)
+        # res_properties.each {|r| schedule_single(r, job)}
+      # end
+#
+      # def schedule_single(res_property, job)
+        # rd = res_property.resource_description
+        # unless type = rd[:type] || rd['type']
+          # raise "Missing resource type in '#{rd}'"
+        # end
+        # res = nil
+        # case type.to_sym
+        # when :node
+          # require 'omf-sfa/resource/node'
+          # res = OMF::SFA::Resource::Node.create(name: res_property.name)
+        # else
+          # raise "Unsupported resource type '#{type}'"
+        # end
+#
+        # unless res
+          # raise "Couldn't create resource '#{rd}'"
+        # end
+        # puts "RES>>> #{res}"
+        # res.job = job
+        # res.save
+        # res_property.resource = res
+        # res_property.save
+#
+      # end
     end
   end
 end
